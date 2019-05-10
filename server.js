10b3356aa31ac935dbf1e24a6ea3df9a2018c7bc
于欢 <huan.yu@datayes.com>
2019-05-10 22:06:10 +0800

    引入nodemon,监控dbs文件中的json文件变化,自动重启服务

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
 const allRoutesKey = Object.keys(db);
+console.log('objectdsfe ----');
 
 server.use(middlewares);
 server.use(router);
 server.listen(serverPort, () => {
-  console.log(`------JSON Server is running: ${serverPort}------- \n`);
+  console.log(`------JSON Server is running: ${serverPort}-------\n`);
   allRoutesKey.forEach((key) => {
     console.log(`http://localhost:${serverPort}/${key} \n`);
   });
