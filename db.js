15e3e401f86179ddf4e33f3b5dadd1e1c06f3cee
于欢 <huan.yu@datayes.com>
2020-03-26 10:33:20 +0800

    yh【babel】引入babel处理import

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
+import _ from 'lodash';
 const { readAllFilesInFolder } = require('./readFiles');
 
-const excludes = ['demo', 'huaan', 'west']; // 要忽略的文件夹
+const excludes = ['demo', 'huaan', 'smline', 'west', 'pms', 'yc']; // 要忽略的文件夹
 const allDBFiles = readAllFilesInFolder('./dbs/', excludes);
-// console.log(allDBFiles);
 
 module.exports = () => {
   const result = {};
